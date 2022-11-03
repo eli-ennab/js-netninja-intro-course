416a2850a2ae246447fbcae131253e1655a46491
Eli Ennab <elinaennab@gmail.com>
2022-11-03 15:30:09 +0100

    Other ways to query the DOM.

--- js intro 44-46/sandbox.js
+++ js intro 44-46/sandbox.js
@@ -6,11 +6,29 @@
 // console.log(para);
 
 // node list in the console
-const paras = document.querySelectorAll('p');
+// const paras = document.querySelectorAll('p');
 // paras.forEach(para => {
 //     console.log(para);
 // });
-console.log(paras [1]);
+// console.log(paras [1]);
 
-const errors = document.querySelectorAll('.error')
-console.log(errors);
+// const errors = document.querySelectorAll('.error')
+// console.log(errors);
+
+/**
+ * other ways to query the DOM
+ */
+
+//  get element by ID
+// const title = document.getElementById('page-title');
+// console.log(title);
+
+// get element by their class name
+// const errors = document.getElementsByClassName('error');
+// console.log(errors);
+// console.log(errors[0]);
+
+// get element by their tag name
+const paras = document.getElementsByTagName('p');
+console.log(paras);
+console.log(paras[1]);

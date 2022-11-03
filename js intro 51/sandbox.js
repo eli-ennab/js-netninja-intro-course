f9b0a2f350d67a99d2fa0066ee85851abcc8a070
Eli Ennab <elinaennab@gmail.com>
2022-11-03 16:22:39 +0100

    Adding & removing classes.

---
+++ js intro 51/sandbox.js
@@ -0,0 +1,18 @@
+/**
+ * Adding & removing classes
+ */
+
+// const content = document.querySelector('p');
+
+// console.log(content.classList);
+
+// // adding classes
+// content.classList.add('error');
+
+// // removing classes
+// content.classList.remove('error');
+
+// // adding another class
+// content.classList.add('success');
+
+

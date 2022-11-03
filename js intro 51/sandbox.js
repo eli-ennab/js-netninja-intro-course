a63fb9172cc9b48c5418c6ef30e51250e1c2e9bf
Eli Ennab <elinaennab@gmail.com>
2022-11-03 16:33:35 +0100

    Adding classes, forEach & toggle.

--- js intro 51/sandbox.js
+++ js intro 51/sandbox.js
@@ -15,4 +15,33 @@
 // // adding another class
 // content.classList.add('success');
 
+/**
+ *  Adding classes
+ */
+
+const paras = document.querySelectorAll('p');
+
+paras.forEach(p =>{
+    // we will get all the visible text available
+    // console.log(p.innerText);
+    // we will get all of the text inside of the tag regardless if it's hidden or not
+    // console.log(p.textContent);
+
+    if (p.textContent.includes('error')) {
+        p.classList.add('error');
+    }
+    if (p.innerText.includes('success')) {
+        p.classList.add('success');
+    }
+})
+
+// Toggle method
+
+const title = document.querySelector('.title');
+
+// we give it the class of 'test' instead of 'title'
+title.classList.toggle('test');
+// if we use it again we remove 'test'
+// title.classList.toggle('test');
+
 
